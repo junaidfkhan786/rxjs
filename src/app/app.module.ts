6270884fef2967f64a57cddddd5fa360ab8b044d
junaidfkhan786 <junaidfkhan786@gmail.com>
2020-08-15 02:45:08 -0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialCustomModule } from 'src/app/material-custom-module/material-custom-module'
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialCustomModule
   ],
   providers: [],
   bootstrap: [AppComponent]
